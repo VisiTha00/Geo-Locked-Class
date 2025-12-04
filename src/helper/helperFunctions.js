f0edd2efb6b6d46f75ac4461519e3561eda4b3f0
Visitha Wickramasinghe <wickramasinghevisitha@gmail.com>
2025-12-04 17:40:08 +0530

    fix: fixed an issue in distance calculation

--- src/helper/helperFunctions.js
+++ src/helper/helperFunctions.js
@@ -6,14 +6,23 @@ export function formatTime(seconds) {
 
 export function getDistanceToSession(activeSession, userLocation) {
   if (!activeSession || !userLocation) return null;
+  if (
+    !activeSession.location ||
+    !activeSession.location.latitude ||
+    !activeSession.location.longitude
+  )
+    return null;
 
   const earthRadius = 6371e3;
+  const sessionLat = activeSession.location.latitude;
+  const sessionLon = activeSession.location.longitude;
+
   const userLatitudeInRadius = (userLocation.latitude * Math.PI) / 180;
-  const sessionLatitudeInRadius = (activeSession.latitude * Math.PI) / 180;
+  const sessionLatitudeInRadius = (sessionLat * Math.PI) / 180;
   const deltaLatitudeInRadius =
-    ((activeSession.latitude - userLocation.latitude) * Math.PI) / 180;
+    ((sessionLat - userLocation.latitude) * Math.PI) / 180;
   const deltaLongitudeInRadius =
-    ((activeSession.longitude - userLocation.longitude) * Math.PI) / 180;
+    ((sessionLon - userLocation.longitude) * Math.PI) / 180;
 
   const a =
     Math.sin(deltaLatitudeInRadius / 2) * Math.sin(deltaLatitudeInRadius / 2) +
