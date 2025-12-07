949f630b9235283d5877ba51af144855309c5416
Visitha Wickramasinghe <wickramasinghevisitha@gmail.com>
2025-12-07 21:54:36 +0530

    fix: fixed offline storage issue

--- src/service/offlineStorageService.js
+++ src/service/offlineStorageService.js
@@ -1,5 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { OfflineSubmission } from "../models/SessionTypes";
+import { OfflineSubmission } from "../types/sessionTypes";
 
 class OfflineStorageService {
   constructor() {
